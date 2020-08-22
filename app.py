98562407de7871a1236c0b557d73aa64709e068f
Kwon <sanghyukkwon90@gmail.com>
2020-08-22 01:37:25 -0400

    request inputs

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import requests
 from flask import Flask
 # import sys
-# Creating variables sent in the rquest
+# Creating variables sent in the request
 
 # utils
 mapDict = {
@@ -27,25 +27,24 @@ def index():
     return '<h1>hello</h1>'
 
 @app.route('/generateChart')
-def generateChart(ticker,label, start_date,end_date):
+def generateChart():
+    ticker = requests.args.get('ticker')
+    start_date = requests.args.get('start_date')
+    end_date = requests.args.get('end_date')
+    label = requests.args.get['chart_type']
     print('ahhhhhh')
     df = quandl.get_table('SHARADAR/DAILY', ticker=ticker) 
-
     # Parsing to the pandas label
     method = mapDict[label]
-    
     # Mapping rolling average
     df["30-day Moving Average"] = df[method].rolling(window=30).mean()
     df["60-day Moving Average"] = df[method].rolling(window=60).mean()
-
     # Create a boolean in the time range after parsing string into datetime
     start_time = parseTime(start_date)
     end_time = parseTime(end_date)
-    mask = (df['date'] > start_time) & (df['date'] <= end_time)
-    
+    mask = (df['date'] > start_time) & (df['date'] <= end_time) 
     # Generate average
     df["Average for Period"] = df.loc[mask][method].mean()
-
     #Chart
     plt.xlabel("Date")
     plt.ylabel(label)
